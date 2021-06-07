6047071534a78b5e4cbbbbcdc0cf3e714dd3b95f
Kangming Deng <dengkangming@gmail.com>
2021-06-07 19:24:47 -0400

    Add and refactor customer and employee routines as separate blend trees

--- scripts/Penguins/AnimationTree.gd
+++ scripts/Penguins/AnimationTree.gd
@@ -1,15 +1,28 @@
 extends AnimationTree
 
+
 func _ready():
-	self.active = true
+	# Default Non-Zero Parameters
+	# Penguin
+	self.set("parameters/mouth/add_amount", 1)
+	self.set("parameters/walk loop/add_amount", 1)
+	# Customer
+	self.set("parameters/Customer/sit loop/add_amount", 1)
+	# Chef
+	self.set("parameters/Chef/cook deep fryer left and right/add_amount", 1)
+	self.set("parameters/Chef/cook stove left and right/add_amount", 1)
+	# Waiter
+	self.set("parameters/Waiter/waiter right and left/add_amount", 1)
+	self.active = true	
 
 
 func walk_or_sit(blend_amount: float):
-	self.set("parameters/walk or sit/blend_amount", blend_amount)
+	# TODO: Refactor to accomodate employee routines.
+	self.set("parameters/walk or routine/blend_amount", blend_amount)
 
 
 func walk_arms_still_or_sway(blend_amount: float):
-	self.set("parameters/walk look upper/blend_amount", blend_amount)
+	self.set("parameters/walk loop upper/blend_amount", blend_amount)
 
 
 func sit_arms_still_or_flap(blend_amount: float):
