71bc67c307172db1c13b57da56fc415251b301a4
Amy Yu <amyyu@cs.stonybrook.edu>
2021-07-04 18:54:59 -0400

    Add Fishing Minigame Elements

--- scripts/Fishing.gd
+++ scripts/Fishing.gd
@@ -1,5 +1,33 @@
 extends "res://scripts/SceneManager/SceneWorker.gd"
 
+onready var StartPanel = $StartPanel
+onready var ProgressBar = $ProgressBar
+
+var game = false
+var status = 0
+
 
 func _on_BackButton_pressed():
 	resign()
+
+
+func _start_game():
+	StartPanel.visible = false
+	game = true
+	status = 0
+	ProgressBar.set_value(0)
+
+
+func _on_Button_pressed():
+	ProgressBar.set_value(ProgressBar.get_value() + 2)
+
+func _process(_delta):
+	if game:
+		ProgressBar.set_value(ProgressBar.get_value() - 0.1)
+		if ProgressBar.get_value() < 60 and ProgressBar.get_value() > 50:
+			status += 1
+		else:
+			status = 0
+		if status >= 500:
+			game = false
+			StartPanel.visible = true
