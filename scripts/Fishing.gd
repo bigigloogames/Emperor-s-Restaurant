1ef245d56b13b44f95ae619da12a6a357090e913
Amy Yu <amyyu@cs.stonybrook.edu>
2021-07-27 17:20:03 -0400

    Replace ProgressBar with TextureProgress

--- scripts/Fishing.gd
+++ scripts/Fishing.gd
@@ -1,11 +1,12 @@
 extends "res://scripts/SceneManager/SceneWorker.gd"
 
 onready var StartPanel = $StartPanel
-onready var ProgressBar = $ProgressBar
+onready var FishingBar = $FishingBar
+onready var FishingCircle = $FishingBar/FishingCircle
 
 var game = false
-var status = 0
-
+var x = -16  # Hardcoded values
+var y = 459  # Change later
 
 func _on_BackButton_pressed():
 	resign()
@@ -14,20 +15,23 @@ func _on_BackButton_pressed():
 func _start_game():
 	StartPanel.visible = false
 	game = true
-	status = 0
-	ProgressBar.set_value(0)
+	FishingBar.set_value(0)
+	FishingCircle.set_value(0)
+	FishingCircle.set_position(Vector2(x, y))
 
 
 func _on_Button_pressed():
-	ProgressBar.set_value(ProgressBar.get_value() + 2)
+	FishingBar.set_value(FishingBar.get_value() + 2)
 
 func _process(_delta):
 	if game:
-		ProgressBar.set_value(ProgressBar.get_value() - 0.1)
-		if ProgressBar.get_value() < 60 and ProgressBar.get_value() > 50:
-			status += 1
+		FishingBar.set_value(FishingBar.get_value() - 0.1)
+		var newPosition = Vector2(x, y - (FishingBar.get_value() * 5))
+		FishingCircle.set_position(newPosition)
+		if FishingBar.get_value() < 80 and FishingBar.get_value() > 60:
+			FishingCircle.set_value(FishingCircle.get_value() + 0.2)
 		else:
-			status = 0
-		if status >= 500:
+			FishingCircle.set_value(0)
+		if FishingCircle.get_value() == 100:
 			game = false
 			StartPanel.visible = true
