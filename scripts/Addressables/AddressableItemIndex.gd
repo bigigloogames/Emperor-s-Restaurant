28b3068d66d87017d8d8097201c7e864ea3630a1
Kangming Deng <dengkangming@gmail.com>
2021-08-02 16:47:21 -0400

    Fix regression where all appliances are classified as addressable items

--- scripts/Addressables/AddressableItemIndex.gd
+++ scripts/Addressables/AddressableItemIndex.gd
@@ -1,8 +1,10 @@
 extends Reference
 
 const AddressableItem = preload("res://scripts/Addressables/AddressableItem.gd")
-const Orientation = preload("res://scripts/Orientation.gd")
 const Appliances = preload("res://scenes/Appliances.tscn")
+const TYPES = [
+	"Stove"
+]
 
 var scenes = {}
 
@@ -21,9 +23,10 @@ func new_addressable_item(
 ):
 	var item_name_array = item_name.capitalize().split(" ")
 	var item_scene = item_name_array[-1]
+	var item_type = item_name_array[-2]
 	var addressable_item = null
 
-	if item_scene in scenes:
+	if item_scene in scenes and item_type in TYPES:
 		addressable_item = AddressableItem.new()
 		addressable_item.initialize(
 			item_id,
